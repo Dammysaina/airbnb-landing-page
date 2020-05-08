8ea60d6e6b5ee9b3bd18fd76e3286363f260f909
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2020-05-09 00:30:15 +0100

    Headerjs and css

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header.js';
+import Stay from './stay.js';
+
+
+const App = (props) => {
+  return(
+    <div className= "contain">
+      <Header/>
+      <Stay />
+    </div>
+  )
+};
+
+
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Header.css';
+
+
+const Header = (props) => {
+    return (
+        <div className="container">
+            <div>
+                <img src="../images/airbnb.png"/>
+            </div>
+            <div>
+                <ul>
+                    <li>Host a home</li>
+                    <li>Host an experience</li>
+                    <li>Help</li>
+                    <li>Log in</li>
+                    <button>Sign up</button>
+                </ul>
+            </div>
+                
+        </div>
+    )
+};
+
+export default Header;
